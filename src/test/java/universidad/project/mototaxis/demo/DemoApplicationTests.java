e441649f751bb01c303b1e367bdffa5e24a75df1
Salvador Valladares <salva.1416@gmail.com>
2020-05-14 21:19:08 -0600

    Create project

---
+++ src/test/java/universidad/project/mototaxis/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package universidad.project.mototaxis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
