f12bf74c31d533cf97fe4fe1841c63c59255d32b
Salvador Valladares <salva.1416@gmail.com>
2020-05-19 19:35:21 -0600

    Controlador de piloto

--- src/main/java/universidad/project/mototaxis/controllers/PilotoController.java
+++ src/main/java/universidad/project/mototaxis/controllers/PilotoController.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import universidad.project.mototaxis.config.UrlBaseApi;
 import universidad.project.mototaxis.domains.Piloto;
+import universidad.project.mototaxis.domains.Usuario;
 import universidad.project.mototaxis.services.IPilotoService;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(UrlBaseApi.URL_API)
@@ -38,6 +40,35 @@ public class PilotoController {
 
         response.put("size", objNew.size());
         response.put("rows", objNew);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping(entidad)
+    public ResponseEntity<?> create(@Valid @RequestBody Piloto x, BindingResult result) {
+
+        Piloto objNew = null;
+
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors().stream().map(err -> {
+                return "El campo '" + err.getField() + "' " + err.getDefaultMessage();
+            }).collect(Collectors.toList());
+
+            response.put("errors", errors);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            objNew = objService.create(x);
+
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al insertar en la base de datos");
+            response.put("error", ex.getMessage().concat(": ").concat(ex.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "OK");
+        response.put("RES", objNew);
 
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
