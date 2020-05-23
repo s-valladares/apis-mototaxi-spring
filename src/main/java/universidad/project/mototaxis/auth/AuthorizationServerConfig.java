867333361c8237aa018c145c54223a3ffa068a10
Salvador Valladares <salva.1416@gmail.com>
2020-05-23 12:00:57 -0600

    Cambiar clave para grant_type

--- src/main/java/universidad/project/mototaxis/auth/AuthorizationServerConfig.java
+++ src/main/java/universidad/project/mototaxis/auth/AuthorizationServerConfig.java
@@ -40,7 +40,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient("ionicapp")
-                .secret(passwordEncoder.encode("12345"))
+                .secret(passwordEncoder.encode("api.mototaxi.6781"))
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
                 .accessTokenValiditySeconds(3600)
